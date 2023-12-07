60ac90347fada4256f229716a733b84bf84b6015
MILIKITO <108524448+JPmikilita@users.noreply.github.com>
2023-12-07 08:48:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-alert("olá homohabilis primata incopetente e burro");
+function tocasom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeteclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+while(contador < listaDeteclas.length){
+    const efeito = listaDeteclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeteclas[contador].onclick = function(){
+        tocasom (idAudio)
+    }
+    contador = contador + 1
+    console.log(contador);
+
+    listaDeteclas.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+        listaDeteclas.classList.add('ativa')
+        }
+    }
+    listaDeteclas.onkeyup = function(){
+        listaDeteclas.classList.remove('ativa')
+    }
+}
